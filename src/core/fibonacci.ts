85689b61a4224c37cfdddf25940ce8711a0b7531
Silvio <silviosisa@gmail.com>
2025-06-19 07:49:41 -0300

    green is a series where the value for a number is the addition of the preceding two values

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -1,4 +1,5 @@
 export function fibonacci(number: number): number {
     if (number === 0) return 0;
-    return 1;
+    if (number <= 1) return 1;
+    return fibonacci(number - 1) +fibonacci(number - 2);
 }
