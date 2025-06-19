1a8523068df40fc829518a10a08cd9a4dead9e9f
Silvio <silviosisa@gmail.com>
2025-06-19 07:26:17 -0300

    red:yields value one to number one

--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -6,4 +6,7 @@ describe('The fibonacci sequence', () => {
     it('yields value zero to number zero', () => {
         expect(fibonacci()).toBe(0)
     })
+    it('yields value one to number one', () => {
+        expect(fibonacci()).toBe(1)
+    })
 })
